5414f43ac032a02f3638e8c6acc5bf797ba0c4c2
saasbook <saasbook@saasbook.(none)>
2012-10-04 13:47:12 -0700

    test palindrome code, writing

--- part1.rb
+++ part1.rb
@@ -1,33 +1,29 @@
-
-#test_values=['I contain FOUR words', 'Two words']
-#puts test_values
-test_values=['anana', 'naan', 'foo', 12321, 'Cain:a maniac']
-
 def palindrome?(str)
 @string=str.to_s.gsub(/\W+/,'').downcase
 @string==@string.reverse
 end
 
-#test_values.each do |str|
-#puts str
-#answer=palindrome?(str)
-#puts answer
+
+#def count_words(str)
+#@cnt=0
+#@string=str.to_s.scan(/\w+/) do |word|
+#     @cnt+=1
 #end
-
-
+#return @cnt
+#end
+palindrome?("A man, a plan, a canal -- Panama")
+palindrome?("Madam, I'm Adam!")
+palindrome?("Abracadabra")
 def count_words(str)
-@cnt=0
-@string=str.to_s.scan(/\w+/) do |word|
-     @cnt+=1
+@word_hash=Hash.new
+@word=str.to_s.scan(/\b+/) do |b|
+@word_hash[b].has_key?
 end
-return @cnt
-end
-
-test_values.each do |str|
-puts str
-answer=count_words(str)
-puts answer
+puts @word_hash
 end
 
 
 
+count_words("A man, a plan, a canal -- Panama")
+## => {'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1}
+count_words "Doo bee doo bee doo"
