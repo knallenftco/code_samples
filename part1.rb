3666dc80b93d33582ea21e1ae1bc48057c73f2a2
saasbook <saasbook@saasbook.(none)>
2012-10-04 18:38:35 -0700

    completed code for count_words

--- part1.rb
+++ part1.rb
@@ -4,26 +4,23 @@ def palindrome?(str)
 end
 
 
-#def count_words(str)
-#@cnt=0
-#@string=str.to_s.scan(/\w+/) do |word|
-#     @cnt+=1
-#end
-#return @cnt
-#end
 palindrome?("A man, a plan, a canal -- Panama")
 palindrome?("Madam, I'm Adam!")
 palindrome?("Abracadabra")
 def count_words(str)
-@word_hash=Hash.new
-@word=str.to_s.scan(/\b+/) do |b|
-@word_hash[b].has_key?
+@word_hash=Hash.new()
+str.to_s.downcase.scan(/\w+/) do |w|
+if(@word_hash.has_key?(w))
+  @word_hash[w]+=1
+else
+  @word_hash[w]=1
 end
-puts @word_hash
+end
+@word_hash.each {|key,value| puts "#{key} => #{value}"}
 end
 
 
 
 count_words("A man, a plan, a canal -- Panama")
-## => {'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1}
+count_words("I for an i")
 count_words "Doo bee doo bee doo"
