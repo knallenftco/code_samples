8a334e710d25578c0b748dda030dff379e27e341
saasbook <saasbook@saasbook.(none)>
2012-10-06 16:26:14 -0700

    finished tournament

--- part2.rb
+++ part2.rb
@@ -32,11 +32,11 @@ end
 #puts "And the winner is => #{@winner}"
 
 if @winner==1
-  puts "#@winner"
-  puts game[1]
+#  puts "#@winner"
+#  puts game[1][0]
   return game[1]
 else
-  puts game[0]
+#puts game[0][0]
   return game[0]
 end
 end
@@ -44,9 +44,86 @@ end
 rps_game_winner(testgame);
 
 
+my_tournament_full=[
+[
+[  
+  [["Andy","R"],["Kristen","P"]],
+  [["Dick","P"],["Maria","S"]],
+],
+[  
+  [["Heather","R"],["Mike","P"]],
+  [["Kelly","P"],["Bryan","S"]],
+]
+],
+  
+[
+[  
+  [["Broene","R"],["Jonas","P"]],
+  [["Rich","P"],["Jan","S"]]
+],
+[    
+  [["Jim","R"],["MaryAnn","P"]],
+  [["Oscar","P"],["Mateo","S"]]
+]
+]
+]
 
+my_tournament=[
+[  
+  [["Andy","R"],["Kristen","P"]],
+  [["Dick","P"],["Maria","S"]],
+],
+[
+  [["Heather","R"],["Mike","P"]],
+  [["Kelly","P"],["Bryan","S"]],
+]
+]
+
+my_short_tourn=[
+["Andy", "R"],["Kristen","P"]
+]
+
+my_short_tourn2=[
+[["Andy", "R"],["Kristen","P"]],
+[["Kelly", "P"],["Bryan","S"]]
+]
 
 
 def rps_tournament_winner(tournament)
-  # YOUR CODE HERE
+puts "#{tournament}"
+if (tournament[0][1]=~/^[a-zA-Z]$/)
+   print "I am at a leaf\n"
+   rps_game_winner(tournament)
+   #I am at a two person level
+else
+   puts "left half is : #{tournament[0]}"
+   puts "right half is : #{tournament[1]}"
+   left_winner=rps_tournament_winner(tournament[0])
+   puts "the left half winner is: #{left_winner}"
+   right_winner=rps_tournament_winner(tournament[1])
+   puts "the right half winner is:  #{right_winner}"
+   winners = [left_winner,right_winner]
+   puts "the winners bracket is:  #{winners}"
+   winner=rps_game_winner(winners)
+   puts "#{winner}"
+   rps_game_winner(winners)
+#   current_winner=rps_game_winner(winner)
 end
+#puts "The winner is: #{current_winner}"
+end 
+
+#puts "The winner is #{@winner}"
+#end
+
+#tournament.each do |b|
+# puts "operating on this list: #{b}"
+#  if (b[0][1]=~/^[a-zA-Z]$/)
+#  # identified a leaf game
+#  puts "#{b[0][0]} vs #{b[1][0]}"
+#  @game_winner=rps_game_winner(b) # we have a winner of the game
+#  puts "The winner of this game is: #{@game_winner}" #  @bracket_winners<<@game_winner #  rps_game_winner(b) #  leaf_count=+1 #  puts leaf_count #  else #  rps_game_winner(rps_tournament_winner(b)) #  end
+#  puts "Current bracket contains: #{@bracket_winners}" #  if @bracket_winners.length==2 #  @bwn=rps_game_winner(@bracket_winners) #  puts "The winner of this bracket is : #{@bwn}" #  end
+#  end
+#end
+
+rps_tournament_winner(my_tournament_full)
