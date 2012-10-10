7afd41af48180fdb8114f693fc1562a631526791
saasbook <saasbook@saasbook.(none)>
2012-10-10 06:11:45 -0700

    Finished part3.rb

--- part3.rb
+++ part3.rb
@@ -1,3 +1,24 @@
+my_words=['cars','for','four','potatoes','racs','Scar','creams','Scream']
+
 def combine_anagrams(words)
- # YOUR CODE HERE
+hash=Hash.new
+sorted=[]
+
+words.each do |w|
+sorted=w.downcase.chars.sort.join
+#puts "#{sorted}"
+
+if (hash[sorted]==nil)
+hash[sorted]= Array.new
+hash[sorted]<<w;
+else
+hash[sorted]<<w;
 end
+puts "#{w} :sorts to: #{sorted} :entered as: #{hash[sorted]}\n"
+end
+
+#puts "The hash contains Keys: #{hash.keys} and Values: #{hash.values}\n"
+puts"#{hash.values}"
+end
+
+combine_anagrams(my_words)
