5f9a0a736ffd37c82f43d5889c0ff7ac53d8b09b
saasbook <saasbook@saasbook.(none)>
2012-10-10 08:33:41 -0700

    Coded Dessert and JellyBean with return values

--- part4.rb
+++ part4.rb
@@ -1,23 +1,72 @@
 class Dessert
+attr_reader :name
+attr_writer :calories
   def initialize(name, calories)
-    # YOUR CODE HERE
+  @name=name
+  @calories=calories
   end
+
   
   def healthy?
-    # YOUR CODE HERE
+  if @calories<200
+    #puts "Debug: Calories are #{@calories}"
+    return true
+    end
   end
   
   def delicious?
-    # YOUR CODE HERE
+  if @name==nil
+    #puts "Debug: #{@name} is empty"
+    return false
+  else
+    #puts "Debug: #{@name} is a delicious dessert"
+    return true
+  end
   end
 end
 
+
 class JellyBean < Dessert
+    attr_accessor :flavor
   def initialize(name, calories, flavor)
-    # YOUR CODE HERE
+    @name=name
+    @calories=calories
+    @flavor=flavor
   end
-  
   def delicious?
-    # YOUR CODE HERE
+  if @flavor=="black licorice"
+    return false
+  else
+  return true
+  end
   end
 end
+
+#kdessert=Dessert.new("brownie",197);
+#adessert=JellyBean.new("jelly bean1",201,"mint");
+#monkeydessert=JellyBean.new("jelly bean2",199,"black licorice");
+
+#if (kdessert.healthy?)
+#  puts "Dessert: #{kdessert.name} is less than 200 calories!"
+#end
+#if (kdessert.delicious?)
+#  puts "Dessert: #{kdessert.name} is a delicious dessert!"
+#end
+
+
+#if (adessert.healthy?)
+#  puts "Dessert: #{adessert.name} is less than 200 calories!"
+#end
+#if (adessert.delicious?)
+#  puts "Dessert: #{adessert.name} is a delicious dessert!"
+#  puts "Dessert: #{adessert.flavor} is a valid flavor!"
+#end
+#
+#if (monkeydessert.healthy?)
+#  puts "Dessert: #{monkeydessert.name} is less than 200 calories!"
+#end
+#if (monkeydessert.delicious?)
+#   puts "Dessert: #{monkeydessert.name} is a delicious dessert!"
+#   puts "Dessert: #{monkeydessert.flavor} is a valid flavor!"
+#end
+
